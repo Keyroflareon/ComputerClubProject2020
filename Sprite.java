04b9df634891f3c9dbd77b42b9e41070848082c2
Keyroflareon <55201156+Keyroflareon@users.noreply.github.com>
2020-01-06 14:53:08 -0500

    Update Sprite.java

--- Sprite.java
+++ Sprite.java
@@ -1,12 +1,25 @@
+import java.awt.*;
+import java.applet.*;
+
 public class Sprite {
   
   private int health;
+  private ImageEntity entity;
+  boolean int isAlive;
+  private int pos;
   //add image implementation
   
-  public Sprite(int h) {
+  public Sprite(Applet a, Graphics2D g2d, int h) {
+    entity = new ImageEntity(a);
+    entity.setGraphics(g2d);
+    entity.setAlive(false);
+    this.pos = newPoint2D(0, 0);
     health = h;
+    
   }
   
   public boolean isCollided() {//requires implemetation
   }
+  
+  
 }
