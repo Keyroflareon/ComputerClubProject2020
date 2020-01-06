8b69967f645f8faae458db96b6af7f066334af6a
Keyroflareon <55201156+Keyroflareon@users.noreply.github.com>
2020-01-06 14:41:52 -0500

    Sprite.java
    Requires implementation

---
+++ Sprite.java
@@ -0,0 +1,12 @@
+public class Sprite {
+  
+  private int health;
+  //add image implementation
+  
+  public Sprite(int h) {
+    health = h;
+  }
+  
+  public boolean isCollided() {//requires implemetation
+  }
+}
